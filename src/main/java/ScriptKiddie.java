88eb047464db17b28367a51c2c1792c24cc02793
pinwen <eng.pin.wen@gmail.com>
2025-09-03 02:49:10 +0800

    ScriptKiddie will now echo user input

--- src/main/java/ScriptKiddie.java
+++ src/main/java/ScriptKiddie.java
@@ -1,14 +1,32 @@
-// Level 0
+// Level 1
 
-// Rename Duke to ScriptKiddie
+import java.util.Scanner;
+
+/*
+ * Echo user input
+ * Exit when user input == "bye"
+ */
 public class ScriptKiddie {
     public static void main(String[] args) {
         String line = "________________________________";
         String botName = "ScriptKiddie";
+        Scanner userInput = new Scanner(System.in);
+        String currUserInput = "";
+
         System.out.printf("%s\n", line);
         System.out.printf("Hello! I'm %s\n", botName);
         System.out.print("What can I do for you?\n");
         System.out.printf("%s\n", line);
+
+        do {
+            currUserInput = userInput.nextLine();
+            System.out.printf("%s\n", line);
+            if (!currUserInput.equals("bye")) {
+                System.out.printf("%s\n", currUserInput);
+                System.out.printf("%s\n", line);
+            }
+        } while (!currUserInput.equals("bye"));
+
         System.out.print("Bye. Hope to see you again soon!\n");
         System.out.printf("%s\n", line);
     }
