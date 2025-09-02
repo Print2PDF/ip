98c7e659af289c1771cbc18315af7964600e24a9
pinwen <eng.pin.wen@gmail.com>
2025-09-02 23:38:29 +0800

    Renamed Duke to ScriptKiddie and added a basic message

--- src/main/java/ScriptKiddie.java
+++ src/main/java/ScriptKiddie.java
@@ -1,5 +1,15 @@
+// Level 0
+
+// Rename Duke to ScriptKiddie
 public class ScriptKiddie {
     public static void main(String[] args) {
-        
+        String line = "________________________________";
+        String botName = "ScriptKiddie";
+        System.out.printf("%s\n", line);
+        System.out.printf("Hello! I'm %s\n", botName);
+        System.out.print("What can I do for you?\n");
+        System.out.printf("%s\n", line);
+        System.out.print("Bye. Hope to see you again soon!\n");
+        System.out.printf("%s\n", line);
     }
 }
